0ef5bfad81169b315c4fbcf47f4b3b6488d5cac8
Alexis Marembert <graguelame@gmail.com>
2017-04-08 23:23:21 +0200

    Add files via upload

---
+++ api/posts.php
@@ -0,0 +1,80 @@
+﻿<?php
+
+	// Pour les accents
+	header('Content-Type: text/html; charset=utf-8');
+	//¨Pour la mise en page
+	header('Content-Type: application/json');
+
+	// On vérifie si un id de post est donné
+	if (isset($_SERVER['PATH_INFO'])) {
+		
+		// On récupère l'ID
+		$id = intval(substr($_SERVER['PATH_INFO'], 1));
+	  
+		// On affiche le post si il existe, sinon une erreur
+		if (file_exists("saved/$id.json")) {
+			echo '{"post":';
+			readfile("saved/$id.json");
+			echo '}';
+		} else {
+			echo '{"error":"id not found!"}';
+		}
+		
+	} else {
+		
+		// On récupère les paramètres
+		$from = null;
+		if (isset($_GET['from'])) {
+			$from_elements = explode('-', $_GET['from']);
+			$from = mktime(0, 0, 0, intval($from_elements[1]), intval($from_elements[2]), intval($from_elements[0])); // Comme le paramètre from donne un jour, on l'inclu en mettant minuit comme heure
+		}
+		$to = null;
+		if (isset($_GET['to'])) {
+			$to_elements = explode('-', $_GET['to']);
+			$to = mktime(23, 59, 59, intval($to_elements[1]), intval($to_elements[2]), intval($to_elements[0])); // Comme le paramètre to donne un jour, on l'inclu en mettant la dernière seconde de la journée
+		}
+		$author = null;
+		if (isset($_GET['author'])) {
+			$author = $_GET['author'];
+		}
+		
+		// On lit tous les messages
+		$count = 0; // Nombre de messages selectionnés
+		$posts = array("posts" => array());
+		for ($i = 1; $i <= 200; $i++) {
+			
+			// On récupère le json
+			$json = json_decode(file_get_contents("saved/$i.json"));
+			
+			// On récupère le timestamp du post
+			$ts = strtotime($json->date);
+			
+			// On vérifie la date de départ (on exclura le post si il est plus vieux) (si néscessaire)
+			if ($from !== null && $ts < $from) {
+				continue;
+			}
+			
+			// On vérifie la date d'arrivée (on exclura le post si il est plus récent) (si néscessaire)
+			if ($to !== null && $ts > $to) {
+				continue;
+			}
+			
+			// On vérifie l'auteur (on exclura le post si il n'est pas de cet auteur)
+			if ($author !== null && $author != $json->author) {
+				continue;
+			}
+			
+			// Si on arrive ici, cela signifie que le post satisfait toutes les conditions, on peut donc l'ajouter à la liste
+			$posts["posts"][$count] = $json;
+			$count++;
+			
+		}
+		
+		// On ajoute le nombre de posts trouvés
+		$posts["count"] = $count;
+		
+		// On affiche le json
+		echo json_encode($posts, JSON_PRETTY_PRINT);
+		
+	}
+?>
---
+++ api/update.php
@@ -0,0 +1,65 @@
+﻿<?php
+
+	// Pour les accents
+	header('Content-Type: text/html; charset=utf-8');
+
+	// On charge la bibliothèque d'analyse HTML
+	require_once "simple_html_dom/simple_html_dom.php";
+
+	// URL du site VDM
+	$url = 'http://www.viedemerde.fr/?page=';
+	
+	// Tableau pour stocker les données
+	$posts = array();
+	
+	// Comme chaque page retourne 10 VDMs, on le fait 10 fois
+	for ($page = 1; $page <= 20; $page++) {
+
+		// On récpère le contenu
+		$content = file_get_html($url.$page);
+
+		// On récupère le contenu des VDMs que l'on met dans un tableau
+		$i = (($page-1)*10)+1;
+		foreach($content->find('article[class=art-panel col-xs-12] p[class=block] a') as $article) {
+			$posts[$i]['content'] = $article->plaintext;
+			$i++;
+		}
+
+		// On récupère les auteurs
+		$i = (($page-1)*10)+1;
+		foreach($content->find('article[class=art-panel col-xs-12] div[class=text-center]') as $author) {
+			$posts[$i]['author'] = substr(array_values(explode('-', $author->plaintext))[0], 5, -1);
+			$i++;
+		}
+
+		// On récupère les auteurs dates
+		$i = (($page-1)*10)+1;
+		$months = array("janvier"=>1, "février"=>2, "mars"=>3, "avril"=>4, "mai"=>5, "juin"=>6, "juillet"=>7, "août"=>8, "septembre"=>9, "octobre"=>10, "novembre"=>11, "décembre"=>12);
+		foreach($content->find('article[class=art-panel col-xs-12] div[class=text-center]') as $date) {
+			
+			// On formatte la date
+			$vdm_date = substr(array_values(explode('/', $date->plaintext))[1], 1, -1);
+			$re = '/([a-z]+) ([0-9]+) ([a-zéû]+) ([0-9]{4}) ([0-9]{1,2}):([0-9]{1,2})/i'; // Expression régulière pour les dates du site VDM
+			preg_match_all($re, $vdm_date, $matches, PREG_SET_ORDER, 0);
+			$timestamp = mktime($matches[0][5], $matches[0][6], 0, $months[$matches[0][3]], $matches[0][2], $matches[0][4]); // Comme on ne connait pas les secondes, on met 0
+			
+			$posts[$i]['date'] = date("Y-m-d H:i:s", $timestamp);
+			$i++;
+		}
+		
+	}
+	
+	// On enregistre les données en JSON dans des fichiers
+	if (!is_dir("saved")) {
+		mkdir("saved");
+	}
+	for ($i = 1; $i <= 200; $i++) {
+		
+		$posts[$i]['id'] = $i;
+		file_put_contents("saved/$i.json", json_encode($posts[$i]));
+		
+	}
+	
+	echo "done.";
+
+?>
